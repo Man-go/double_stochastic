c5fefa57421737754f064718df6d6594e288a37f
Man-go <insmer@gmail.com>
2020-05-31 13:04:21 +0200

    First commit

---
+++ stochastik.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define n 7 //pocet_riadkov
+#define m 7 //pocet_stlpcov
+#define N 15 
+
+typedef struct {
+	unsigned int rows;
+	unsigned int cols;
+	float* elem;
+}MAT;
+
+
+int main() {
+	srand((unsigned int)time(NULL));
+
+	printf("Hellow Fucking World");
+
+	getchar();
+	return 0;
+}
